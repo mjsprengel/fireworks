168f3cac3d6de90350db15cf0d0dc595d75ee690
mjsprengel <mjsprengel713@gmail.com>
2017-03-04 19:11:14 -0600

    Create sketch.js

---
+++ sketch.js
@@ -0,0 +1,33 @@
+var fireworks = [];
+var gravity;
+
+function setup() {
+  createCanvas(document.documentElement.clientWidth, document.documentElement.clientHeight); 
+  document.documentElement.style.overflow = 'hidden'; //hide scroll bars for better viewing chrome/firefox
+  document.body.scroll = "no"; //hide scrollbars for ie
+  gravity = createVector(0, 0.08);
+  stroke(255);
+  strokeWeight(4); 
+}
+
+function draw() {
+  background(80);
+  //if(random(1) < 0.01){
+    //fireworks.push(new Firework());
+  //}
+  
+  for(var i = fireworks.length-1; i>=0; i--){
+    fireworks[i].update();
+    fireworks[i].show();
+    if(fireworks[i].done()){
+      fireworks.splice(i,1);
+    }
+  }
+
+}
+
+function mousePressed(){
+  fireworks.push(new Firework(mouseX, mouseY));
+}
+
+
